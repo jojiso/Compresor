cc79110507460ae3496587072b390f456260b58c
jojiso <jojiso23@gmail.com>
2016-11-26 20:02:07 +0100

    Verion Inicial

---
+++ compresor.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#Crea un script para Comprimir/Descomprimir ficheros. en formato "tgz" mediante el comando "tar" Mostrará un menú con las opciones:
+#
+# Comprimir
+
+# Descomprimir
+
+# Salir
+
+#Dentro de cada una de las opciones, deberá pedir al usuario los ficheros a comprimir/descomprimir y el destino dónde comprimir/descomprimir.
+
+#También tendrás que subir el script y los desarrollos intermedios al GitHub (indica ruta en el texto en linea)
+
+#funcion para el menu
+menu() {
+	clear
+	echo "--------------"	
+	echo "	Compresor  "
+	echo "--------------"
+	echo "1) Comprimir uno o varios archivo."
+	echo "2) Descomprimir un archivo."
+	echo "3) Salir."
+	echo "--------------------------------"
+	read -p "Seleccione una opción: " opcion
+	return $opcion
+}
+
+#opciones del menu
+opcion(){
+	case $1 in
+		1) read -p "Escribe la ruta de los archivos a comprimir: " ficheros
+		read -p "Escribe el nombre del archivo comprimido: " comprimido
+		comprimir;;
+		2) read -p "Escriba la ruta del archivo que quieres descomprimir con la extension tar.gz: " descomprimir
+		read -p "Escriba la ruta absoluta donde quieras descomprimirlo: " ruta
+		descomprimir;;
+		*) exit;;
+	esac
+}
+
+
+
+#funcion comprimir
+comprimir(){
+	echo "Los ficheros son $ficheros"
+	tar czvf ./$comprimido.tar.gz $ficheros
+}
+
+#funcion descomprimir
+descomprimir(){
+	tar xvfz $descomprimir -C $ruta
+}
+
+while true
+do
+menu 
+opcion $?
+done
+
+

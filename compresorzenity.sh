2cfe06c3161c15d48f1a06aa1299162c280cad96
jojiso <jojiso23@gmail.com>
2016-11-27 13:07:31 +0100

    V.1.0

--- compresorzenity.sh
+++ compresorzenity.sh
@@ -8,18 +8,17 @@
 #Dentro de cada una de las opciones, deberá pedir al usuario los ficheros a comprimir/descomprimir y el destino dónde comprimir/descomprimir.
 #También tendrás que subir el script y los desarrollos intermedios al GitHub (indica ruta en el texto en linea)
 # https://github.com/jojiso/Compresor
+
+destino=`tempfile`
 #funcion para el menu
-menu() {
-	clear
-	echo "--------------"	
-	echo "	Compresor  "
-	echo "--------------"
-	echo "1) Comprimir uno o varios archivo."
-	echo "2) Descomprimir un archivo."
-	echo "3) Salir."
-	echo "--------------------------------"
-	read -p "Seleccione una opción: " opcion
-	return $opcion
+menu(){
+	opcion=`zenity --list --height="210"\
+  --title="Elija la operacion que quiere realizar" \
+  --column="Número operacion" --column="Operacion" \
+	"1" "Comprimir" \
+	"2" "Descomprimir" \
+	"3" "Salir" 2>/dev/null`
+	return $opcion	
 }
 
 #opciones del menu
@@ -27,35 +26,32 @@ opcion(){
 	case $1 in
 		1) #read -p "Escribe la ruta de los archivos a comprimir: " ficheros
 		ficheros=`zenity --file-selection --multiple --separator=" " --title="Selecciona los ficheros a comprimir" 2>/dev/null`
-		read -p "Escribe la ruta absoluta y el nombre del archivo comprimido: " comprimido
+		ruta=`zenity --file-selection --directory --title="Selecciona la ruta donde comprimir" 2>/dev/null`		
+		nombre=`zenity --entry --title="Compresor" --text="Introduce el nombre del archivo comprimido:" 2>/dev/null`
+		
+		#read -p "Escribe la ruta absoluta y el nombre del archivo comprimido: " comprimido
 		comprimir;;
-		2) read -p "Escribe la ruta del archivo que quieres descomprimir con la extension tar.gz: " descomprimir
-		read -p "Escribe la ruta absoluta donde quieras descomprimirlo: " ruta
+		2) #read -p "Escribe la ruta del archivo que quieres descomprimir con la extension tar.gz: " descomprimir
+		descomp=`zenity --file-selection --title="Selecciona el fichero a descomprimir" 2>/dev/null`		
+		#read -p "Escribe la ruta absoluta donde quieras descomprimirlo: " ruta
+		ruta=`zenity --file-selection --directory --title="Selecciona la ruta donde descomprimir" 2>/dev/null`		
 		descomprimir;;
 		*) exit;;
 	esac
 }
 
-
-
 #funcion comprimir
-comprimir(){
-	for arch in $ficheros
-	do
-	if [ -f $arch ]
-	then
-	echo "El fichero $arch existe"
-	tar czvf $comprimido.tar.gz $ficheros
-	else
-	echo "El fichero $arch no existe"
-fi 
-done
-	read -s
+
+comprimir(){    
+	
+   	tar -czvf $ruta/$nombre.tar.gz $ficheros
+	zenity --info --text="El archivo $nombre.tar.gz se ha comprimido con exito" 2>/dev/null
 }
 
 #funcion descomprimir
 descomprimir(){
-	tar xvfz $descomprimir -C $ruta
+	tar -xzvf $descomp -C $ruta
+	zenity --info --text="$descomp se ha descomprimido en $ruta" 2>/dev/null
 }
 
 while true
